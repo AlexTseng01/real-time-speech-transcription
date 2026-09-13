bd8e02ce0c40bc9b670396365efdb26bf05b1bca
AlexTseng01 <atseng1@umbc.edu>
2026-09-13 01:57:58 -0400

    slighly cleaner ui

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # Use YAMNet for classifying audios like differentiating if an audio is a bird, human, or a dinosaur
-import microphone_transcriber
-import desktop_transcriber
 from datetime import datetime
 from scipy.io.wavfile import write
 import os
@@ -8,10 +6,33 @@ import torch
 from queue import Queue
 import threading
 
+# Initial setup
+transcriber_mode = ""
+start_new_transcript = ""
+
+# Prompt input for recording type
+while transcriber_mode not in ("1", "2"):
+        transcriber_mode = input("Press [1] for Microphone, [2] for Desktop transcription: ")
+
+if transcriber_mode == "1":
+    import microphone_transcriber as transcriber
+else:
+    import desktop_transcriber as transcriber
+
+# Prompt input for new transcription
+while start_new_transcript not in ("y", "n"):
+    start_new_transcript = input("Press [y] for new transcription, [n] for appending: ")
+    
+if start_new_transcript == "y":
+    if os.path.exists("transcript.txt"):
+        os.remove("transcript.txt")
+
 # Forces record() to work without waiting for transcribe()
+# Current bugs:
+# - threads do not terminate using ctrl + c until the next time VAD detects speech
 def record_worker(audio_queue, model, utils, device):
     while True:
-        audio = microphone_transcriber.record(model, utils, device)
+        audio = transcriber.record(model, utils, device)
         audio_queue.put(audio)
 
 # Main loop
@@ -21,25 +42,15 @@ def main():
     model = model.to(device)
 
     audio_queue = Queue()
-    threading.Thread(target=record_worker, args=(audio_queue, model, utils, device), daemon=True).start()
 
-    start_new_transcript = ""
-    while start_new_transcript not in ("y", "n"):
-        start_new_transcript = input("Start new transcription? (y/n): ")
-        
-    if start_new_transcript == "y":
-        if os.path.exists("transcript.txt"):
-            os.remove("transcript.txt")
-        print("[Starting new transcript]")
-    else:
-        print("[Continuing new transcript]")
+    threading.Thread(target=record_worker, args=(audio_queue, model, utils, device), daemon=True).start()
 
     while True:
         audio = audio_queue.get()
         
-        write("temp.wav", microphone_transcriber.SAMPLE_RATE, audio)
+        write("temp.wav", transcriber.SAMPLE_RATE, audio)
 
-        transcription = microphone_transcriber.transcribe("temp.wav")
+        transcription = transcriber.transcribe("temp.wav")
 
         print(f"[{datetime.now().strftime('%H:%M:%S')}]:{transcription.text}\n")
         with open("transcript.txt", "a", encoding="utf-8") as f:
